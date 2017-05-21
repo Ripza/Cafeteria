662449ee959774a2abdfaf35129c96d9ca206ea8
René Zárate <rene.zarate@usach.cl>
2017-05-21 12:14:08 -0400

    Conexión a DB funcionando

--- Cafeteria-ejb/target/generated-sources/annotations/entidades/Comida_.java
+++ Cafeteria-ejb/target/generated-sources/annotations/entidades/Comida_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-20T13:05:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-20T13:46:36")
 @StaticMetamodel(Comida.class)
 public class Comida_ { 
 
