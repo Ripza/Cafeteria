7c3e072988bf2cbe06aa3b953bed7b5cc9e13223
René Zárate <rene.zarate@usach.cl>
2017-06-20 08:29:21 -0400

    Implementado PMD

--- Cafeteria-ejb/target/generated-sources/annotations/entidades/Menus_.java
+++ Cafeteria-ejb/target/generated-sources/annotations/entidades/Menus_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-20T08:21:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-20T08:27:38")
 @StaticMetamodel(Menus.class)
 public class Menus_ { 
 
